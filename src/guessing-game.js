f30e020f3479415cf65f654fdeff99a56f0c098a
“Irina <irina.osipova2012@gmail.com>
2023-02-23 17:14:27 +0100

    feat: task solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.mid;
+        this.min = 0
+        this.max
+    }
 
     setRange(min, max) {
-
+      this.min = min
+      this.max = max
     }
 
     guess() {
-
+        this.mid = Math.round((this.max-this.min)/2+this.min);
+        return this.mid;
     }
 
     lower() {
-
+      this.max = this.mid
     }
 
     greater() {
-
+      this.min = this.mid
     }
 }
 
 module.exports = GuessingGame;
+
+
